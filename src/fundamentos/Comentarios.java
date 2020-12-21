3819432d49c77b861533d31f1a649c469ad0abee
MathSena <MathSena07@hotmail.com>
2020-12-21 08:36:06 -0300

    Primeiras Aulas

---
+++ src/fundamentos/Comentarios.java
@@ -0,0 +1,30 @@
+package fundamentos;
+/**
+ * 
+ * @author Matheus Sena
+ *@since 2020/12/21
+ */
+
+public class Comentarios {
+	
+	public static void main(String[] args) {
+		// Uma sentença termina com  ;
+		System.out.println("Hello World!");
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha3
+		 */
+
+		//Mais uma linha
+		//...
+		System.out.println("The End!"); //Comentário no final da linha
+		
+		/**
+		 * JavaDoc comentário
+		 */
+	}
+
+
+}
